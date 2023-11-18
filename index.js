51d7af1bf30357a80cb6cc89232233b74d5edf05
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-11-18 12:11:13 +0530

    update radio items

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import {catsData} from './data.js'
 
-const emtionRadios = document.getElementByIdJ('emotion-radios')
+const emotionRadios = document.getElementById('emotion-radios')
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e) {
+    document.getElementById(e.target.id).parentElement.classList.add("highlight")
+}
 
 function getEmotionsArray(cats) {
     const emotionsArray = []
